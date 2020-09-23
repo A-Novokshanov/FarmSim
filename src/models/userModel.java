f3c59104608c50c6b3560efcc16764a81d1d8368
anovokshanov3 <anovokshanov3@gatech.edu>
2020-09-23 14:53:25 -0400

    Remove unnecessary default constructor in userModel.java

--- src/models/userModel.java
+++ src/models/userModel.java
@@ -6,31 +6,26 @@ package models;
  * @version 1.0
  */
 public class userModel {
-        /** @param Represents the type of the user. */
-        private String type;
+    /** @param Represents the type of the user. */
+    private String type;
 
-        /** Constructs a User object with specified Type.
-         @param type The type of user
-         */
-        public userModel(String type) {
-            this.type = type;
-        }
+    /** Constructs a User object with specified Type.
+     @param type The type of user
+     */
+    public userModel(String type) {
+        this.type = type;
+    }
 
-        /** Constructs a User object with default Type.
-         */
-        public userModel() {
-            this("Player");
-        }
+    /** @return Gets and returns the Type attribute of a userModel object. */
+    public String getUserType() {
+        return this.type;
+    }
 
-        /** @return Gets and returns the Type attribute of a userModel object. */
-        public String getUserType() {
-            return this.type;
-        }
-        /** Sets the type of a userModel.
-        @param type is the new desired type of a userModel object.
-        */
-        public void setUserType(String type) {
-            this.type = type;
-        }
+    /** Sets the type of a userModel.
+     @param type is the new desired type of a userModel object.
+     */
+    public void setUserType(String type) {
+        this.type = type;
     }
 }
+
