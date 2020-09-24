0567fe70d59169d6d5bb414b911a6af6441297e0
Pratap, Aditya Varun <apratap7@gatech.edu>
2020-09-24 18:12:04 -0400

    Implemented the SettingViewModel class to have the string properties and models.

--- src/viewmodels/SettingViewModel.java
+++ src/viewmodels/SettingViewModel.java
@@ -1,4 +1,52 @@
 package viewmodels;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import models.SeasonModel;
+import models.SeedModel;
+
 public class SettingViewModel {
+
+    private StringProperty playerName = new SimpleStringProperty();
+    private StringProperty startingDifficulty = new SimpleStringProperty();
+    private SeedModel startingSeedType;
+    private SeasonModel startingSeasonType;
+
+    /**
+     * This constructor takes in the seedModel and seasonModel for the particular user.
+     *
+     * @param seedModel   The seed model object of the user.
+     * @param seasonModel The season model object of the user.
+     */
+    public SettingViewModel(SeedModel seedModel, SeasonModel seasonModel) {
+        this.startingSeedType = seedModel;
+        this.startingSeasonType = seasonModel;
+    }
+
+    /**
+     * Gets the player name string property to be binded in the view class.
+     *
+     * @return The player name string property.
+     */
+    public StringProperty getPlayerName() {
+        return this.playerName;
+    }
+
+    /**
+     * Gets the player's starting difficulty string property to be binded in the view class.
+     *
+     * @return The player's starting difficulty string property.
+     */
+    public StringProperty getStartingDifficulty() {
+        return this.startingDifficulty;
+    }
+
+    /**
+     * Packs the data into the SettingModel object and passes to the services package.
+     * Then, deserializes the data and adds to the database.
+     */
+    public void addSettingsToDatabase() {
+        // TODO Add user settings to the database.
+    }
+
 }
