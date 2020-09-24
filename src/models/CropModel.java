7493a9403807b948d1cd47812eff58fd65f55da4
Pratap, Aditya Varun <apratap7@gatech.edu>
2020-09-24 13:31:38 -0400

    Added and implemented the CropModel class which keeps track of a crop's attributes.

--- src/models/CropModel.java
+++ src/models/CropModel.java
@@ -1,4 +1,81 @@
 package models;
 
+/**
+ * This class keeps track of a crop's attributes.
+ *
+ * @author Aditya Varun Pratap
+ * @version 1.0
+ */
 public class CropModel {
+
+    private String cropName;
+    private int cropQuantity;
+    private double cropValue;
+
+    /**
+     * This 3-parameter constructor initializes the instance variables to the parameters.
+     *
+     * @param cropName     The name of the crop.
+     * @param cropQuantity The quantity of the specific crop.
+     * @param cropValue    The value of each crop.
+     */
+    public CropModel(String cropName, int cropQuantity, double cropValue) {
+        this.cropName = cropName;
+        this.cropQuantity = cropQuantity;
+        this.cropValue = cropValue;
+    }
+
+    /**
+     * Gets the name of the crop.
+     *
+     * @return The name attribute of the CropModel object.
+     */
+    public String getCropName() {
+        return cropName;
+    }
+
+    /**
+     * Sets the name of the crop.
+     *
+     * @param cropName The name to be set to the crop name attribute.
+     */
+    public void setCropName(String cropName) {
+        this.cropName = cropName;
+    }
+
+    /**
+     * Gets the quantity of the crop.
+     *
+     * @return The quantity attribute of the CropModel object.
+     */
+    public int getCropQuantity() {
+        return cropQuantity;
+    }
+
+    /**
+     * Sets the quantity of the crop.
+     *
+     * @param cropQuantity The quantity to be set to the crop name attribute.
+     */
+    public void setCropQuantity(int cropQuantity) {
+        this.cropQuantity = cropQuantity;
+    }
+
+    /**
+     * Gets the value of the crop.
+     *
+     * @return The value attribute of the CropModel object.
+     */
+    public double getCropValue() {
+        return cropValue;
+    }
+
+    /**
+     * Sets the value of the crop.
+     *
+     * @param cropValue The value to be set to the crop name attribute.
+     */
+    public void setCropValue(double cropValue) {
+        this.cropValue = cropValue;
+    }
 }
