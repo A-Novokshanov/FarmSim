95d53660f4bd95aaea94617bd992c495d26a99fb
Rohan Kashiviswanathan <rohkash@gatech.edu>
2020-09-24 19:23:39 -0400

    Update Junit files

--- src/junits/SeedTestRohanKashi.java
+++ src/junits/SeedTestRohanKashi.java
@@ -8,16 +8,19 @@ import static org.junit.Assert.assertEquals;
 
 public class SeedTestRohanKashi {
     private SeedModel seedModel;
+
     @Before
     public void setup() {
         this.seedModel = new SeedModel("banana");
     }
 
+    //Test getter method for SeedModel
     @Test
     public void testGetSeedType() {
         assertEquals("banana", this.seedModel.getSeedType());
     }
 
+    //Test setter method for SeedModel
     @Test
     public void testSetSeedType() {
         this.seedModel.setSeedType("apple");
