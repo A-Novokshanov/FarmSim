78d0c53ce96df48d693b5f82545f94b0fb68fec3
sjacob31 <sjacob31@gatech.edu>
2020-09-26 19:37:47 -0400

    Adding Junit for CropModel and resolving merge conflicts

---
+++ src/junits/CropModelTestShaunJacob.java
@@ -0,0 +1,13 @@
+package junits;
+
+import models.CropModel;
+import org.junit.Before;
+
+public class CropModelTestShaunJacob {
+    private CropModel cropModel;
+
+    @Before
+    public void setUp() {
+
+    }
+}
