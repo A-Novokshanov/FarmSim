e2a8852d70419b6a9b203c3fcc58fc5ef1f4fc6b
Pratap, Aditya Varun <apratap7@gatech.edu>
2020-10-10 17:10:54 -0400

    Added storageModel parameter to the constructor.

--- src/viewmodels/StorageViewModel.java
+++ src/viewmodels/StorageViewModel.java
@@ -4,7 +4,7 @@ import models.CropModel;
 import models.StorageModel;
 
 /**
- *This view-model class keeps controls the logic flow of adding to a user's inventory.
+ * This view-model class keeps controls the logic flow of adding to a user's inventory.
  *
  * @author Aditya Varun Pratap
  * @version 1.0
@@ -12,8 +12,13 @@ import models.StorageModel;
 public class StorageViewModel {
     private StorageModel storageModel;
 
-    public StorageViewModel() {
-        storageModel = new StorageModel();
+    /**
+     * This constructor initializes the instance variables.
+     *
+     * @param storageModel The storage model object.l
+     */
+    public StorageViewModel(StorageModel storageModel) {
+        this.storageModel = storageModel;
     }
 
     /**
@@ -34,33 +39,28 @@ public class StorageViewModel {
         }
     }
 
-        /**
-         * Removes item from inventory and returns the removed item.
-         *
-         * @param crop The crop the user is trying to sell.
-         * @param amount The amount of crop the user is trying to sell.l
-         * @return The removed item.
-         */
-        public void sellItemFromInventory (CropModel crop,int amount){
-            if (storageModel.getInventorySize() > 0 && storageModel.upForSale(crop)) {
-                for (int i = 0; i < storageModel.getInventorySize(); i++) {
-                    if (storageModel.checkIfNameCorrect(i, crop)) {
+    /**
+     * Removes item from inventory and returns the removed item.
+     *
+     * @param crop   The crop the user is trying to sell.
+     * @param amount The amount of crop the user is trying to sell.
+     */
+    public void sellItemFromInventory(CropModel crop, int amount) {
+        if (storageModel.getInventorySize() > 0 && storageModel.upForSale(crop)) {
+            for (int i = 0; i < storageModel.getInventorySize(); i++) {
+                if (storageModel.checkIfNameCorrect(i, crop)) {
+                    if (storageModel.getEnoughToRemove(i, amount) == 1) {
                         if (storageModel.getEnoughToRemove(i, amount) == 1) {
-                            if (storageModel.getEnoughToRemove(i, amount) == 1) {
-                                storageModel.removeCropAmount(amount, i);
-                            } else {
-                                storageModel.removeCrop(i);
-                            }
-
+                            storageModel.removeCropAmount(amount, i);
+                        } else {
+                            storageModel.removeCrop(i);
                         }
+
                     }
                 }
             }
         }
-
-
-
-
+    }
 
 
 }
