9b8ac2bbe92094250e07f04e157c7f8229e45bb1
anovokshanov3 <anovokshanov3@gatech.edu>
2020-10-09 20:35:53 -0400

    Add MarketModel.java class

---
+++ src/models/MarketModel.java
@@ -0,0 +1,43 @@
+package models;
+/**
+ The MarketModel class represents the model class of the Market
+ holding the instance variables and associating methods.
+ * @author Andrew Novokshanov
+ * @version 1.0
+ */
+public class MarketModel {
+    private CropModel[] cropsInMarket;
+    private SettingModel settings;
+
+    /** Constructs a MarketModel object.
+     * @param cropsInMarket The list of Crops available in the market
+     */
+    public MarketModel(CropModel[] cropsInMarket, SettingModel settings) {
+        this.cropsInMarket = cropsInMarket;
+        this.settings = settings;
+    }
+
+    /** @return Gets and returns the list of crops in a MarketModel object. */
+    public CropModel[] getCropsInMarket() {
+        return this.cropsInMarket;
+    }
+
+    /** Sets the list of crops in a MarketModel object.
+     @param cropsInMarket is the new desired list of crops of a MarketModel object.
+     */
+    public void setCropsInMarket(CropModel[] cropsInMarket) {
+        this.cropsInMarket = cropsInMarket;
+    }
+
+    /** @return Gets and returns the SettingModel of a MarketModel object. */
+    public SettingModel getSettingModel() {
+        return this.settings;
+    }
+
+    /** Sets the SettingModel of a MarketModel object.
+     @param settings is the new desired SettingModel of a MarketModel object.
+     */
+    public void setSettingModel(SettingModel settings) {
+        this.settings = settings;
+    }
+}
