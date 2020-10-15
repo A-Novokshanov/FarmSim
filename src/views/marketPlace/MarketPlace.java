9d21b6e168f6402c882dece7f77c1876d5db9a71
mfarias6 <mfarias6@gatech.edu>
2020-10-14 20:36:37 -0400

    Set Up Buying and Selling in Market
    The structure and functionality of buying and selling from market have been set up.

--- src/views/marketPlace/MarketPlace.java
+++ src/views/marketPlace/MarketPlace.java
@@ -128,72 +128,250 @@ public class MarketPlace {
 
     public void initData(MouseEvent mouseEvent,
                          PlayerViewModel player, StorageViewModel storageViewModel, String name) {
-        marketViewModel = new MarketViewModel(player);
-        playerViewModel = player;
-        playerName = name;
-        budget.setText(String.valueOf(player.getPlayer().getUserCurrentMoney()));
+        this.marketViewModel = new MarketViewModel(player);
+        this.storageViewModel = storageViewModel;
+        this.playerViewModel = player;
+        this.playerName = name;
+        this.budget.setText("$" + (player.getPlayer().getUserCurrentMoney()));
         if (storageViewModel.userInventory().get(0) != null) {
-            p1Quantity.setText(
-                    String.valueOf(storageViewModel.userInventory().get(0).getCropQuantity()));
+            setPrice(p1Value, 0, p1Price);
+            p1Quantity.setText(doubleDigitString(
+                    storageViewModel.userInventory().get(0).getCropQuantity()));
         }
         if (storageViewModel.userInventory().get(1) != null) {
-            p2Quantity.setText(
-                    String.valueOf(storageViewModel.userInventory().get(1).getCropQuantity()));
+            setPrice(p2Value, 1, p2Price);
+            p2Quantity.setText(doubleDigitString(
+                    storageViewModel.userInventory().get(1).getCropQuantity()));
         }
         if (storageViewModel.userInventory().get(2) != null) {
-            p3Quantity.setText(
-                    String.valueOf(storageViewModel.userInventory().get(2).getCropQuantity()));
+            setPrice(p3Value, 2, p3Price);
+            p3Quantity.setText(doubleDigitString(
+                    storageViewModel.userInventory().get(2).getCropQuantity()));
         }
         sellSwap(mouseEvent);
     }
 
-    private void upValue(Text value, Text price, int crop) {
+    private void upValue(Text value, int crop, Text price, Text quantity) {
         int num = Integer.parseInt(value.getText());
         if (num < 15) {
             num++;
         }
-        String str;
-        if (num < 10) {
-            str = "0" + num;
-        } else {
-            str = String.valueOf(num);
-        }
+        String str = doubleDigitString(num);
         value.setText(str);
-        setPrice(price, crop, value);
+        setPrice(value, crop, price);
     }
 
-    private void downValue(Text value, Text price, int crop) {
+    private void downValue(Text value, int crop, Text price, Text quantity) {
         int num = Integer.parseInt(value.getText());
         if (num > 1) {
             num--;
         }
+        String str = doubleDigitString(num);
+        value.setText(str);
+        setPrice(value, crop, price);
+    }
+
+    private void setPrice(Text value, int crop, Text price) {
+        double basePrice = storageViewModel.userInventory().get(crop).getCropValue();
+        String curDifficulty =
+                playerViewModel.getPlayer().getPlayerSettings().getStartingDifficulty();
+        double calPrice = marketViewModel.calculateCropPrice(basePrice, curDifficulty);
+        price.setText("$" + (calPrice * Integer.parseInt(value.getText())));
+    }
+
+    private void buyValue(Text value, int crop, Text quantity) {
+        int num = Integer.parseInt(value.getText());
+        int iNum = Integer.parseInt(quantity.getText());
+        double basePrice = storageViewModel.userInventory().get(crop).getCropValue();
+        double budgetCheck = playerViewModel.getPlayer().getUserCurrentMoney();
+        if(crop <= 2) {
+            if (num * basePrice <= budgetCheck) {
+                marketViewModel.purchaseItems(storageViewModel.userInventory().get(crop), num);
+                quantity.setText(doubleDigitString(
+                        storageViewModel.userInventory().get(crop).getCropQuantity()));
+                this.budget.setText("$" + (playerViewModel.getPlayer().getUserCurrentMoney()));
+            }
+        }
+    }
+
+    private void sellValue(Text value, int crop, Text quantity) {
+        int num = Integer.parseInt(value.getText());
+        int iNum = Integer.parseInt(quantity.getText());
+        if (crop <= 2) {
+            if(iNum > 0) {
+                storageViewModel.sellItemFromInventory(
+                        storageViewModel.userInventory().get(crop), num);
+                quantity.setText(doubleDigitString(
+                        storageViewModel.userInventory().get(crop).getCropQuantity()));
+                this.budget.setText("$" + (playerViewModel.getPlayer().getUserCurrentMoney()));
+            }
+        }
+    }
+
+    public void upValue1() {
+        upValue(p1Value, 0, p1Price, p1Quantity);
+    }
+
+    public void downValue1() {
+        downValue(p1Value, 0, p1Price, p1Quantity);
+    }
+
+    public void action1() {
+        if (buyState) {
+            buyValue(p1Value, 0, p1Quantity);
+        } else {
+            sellValue(p1Value, 0, p1Quantity);
+        }
+    }
+
+    public void upValue2() {
+        upValue(p2Value, 1, p2Price, p2Quantity);
+    }
+
+    public void downValue2() {
+        downValue(p2Value, 1, p2Price, p2Quantity);
+    }
+
+    public void action2() {
+        if (buyState) {
+            buyValue(p2Value, 1, p2Quantity);
+        } else {
+            sellValue(p2Value, 1, p2Quantity);
+        }
+    }
+
+    public void upValue3() {
+        upValue(p3Value, 2, p3Price, p3Quantity);
+    }
+
+    public void downValue3() {
+        downValue(p3Value, 2, p3Price, p3Quantity);
+    }
+
+    public void action3() {
+        if (buyState) {
+            buyValue(p3Value, 2, p3Quantity);
+        } else {
+            sellValue(p3Value, 2, p3Quantity);
+        }
+    }
+
+    public void upValue4() {
+        upValue(p4Value, 3, p4Price, p4Quantity);
+    }
+
+    public void downValue4() {
+        downValue(p4Value, 3, p4Price, p4Quantity);
+    }
+
+    public void action4() {
+        if (buyState) {
+            buyValue(p4Value, 3, p4Quantity);
+        } else {
+            sellValue(p4Value, 3, p4Quantity);
+        }
+    }
+
+    public void upValue5() {
+        upValue(p5Value, 4, p5Price, p5Quantity);
+    }
+
+    public void downValue5() {
+        downValue(p5Value, 4, p5Price, p5Quantity);
+    }
+
+    public void action5() {
+        if (buyState) {
+            buyValue(p5Value, 4, p5Quantity);
+        } else {
+            sellValue(p5Value, 4, p5Quantity);
+        }
+    }
+
+    public void upValue6() {
+        upValue(p6Value, 5, p6Price, p6Quantity);
+    }
+
+    public void downValue6() {
+        downValue(p6Value, 5, p6Price, p6Quantity);
+    }
+
+    public void action6() {
+        if (buyState) {
+            buyValue(p6Value, 5, p6Quantity);
+        } else {
+            sellValue(p6Value, 5, p6Quantity);
+        }
+    }
+
+    public void upValue7() {
+        upValue(p7Value, 6, p7Price, p7Quantity);
+    }
+
+    public void downValue7() {
+        downValue(p7Value, 6, p7Price, p7Quantity);
+    }
+
+    public void action7() {
+        if (buyState) {
+            buyValue(p7Value, 6, p7Quantity);
+        } else {
+            sellValue(p7Value, 6, p7Quantity);
+        }
+    }
+
+    public void upValue8() {
+        upValue(p8Value, 7, p8Price, p8Quantity);
+    }
+
+    public void downValue8() {
+        downValue(p8Value, 7, p8Price, p8Quantity);
+    }
+
+    public void action8() {
+        if (buyState) {
+            buyValue(p8Value, 7, p8Quantity);
+        } else {
+            sellValue(p8Value, 7, p8Quantity);
+        }
+    }
+
+    private void setActionLabel(String str) {
+        p1Action.setText(str);
+        p2Action.setText(str);
+        p3Action.setText(str);
+        p4Action.setText(str);
+        p5Action.setText(str);
+        p6Action.setText(str);
+        p7Action.setText(str);
+        p8Action.setText(str);
+    }
+
+    private void resetValues() {
+        p1Value.setText("01");
+        p2Value.setText("01");
+        p3Value.setText("01");
+        p4Value.setText("01");
+        p5Value.setText("01");
+        p6Value.setText("01");
+        p7Value.setText("01");
+        p8Value.setText("01");
+    }
+
+    private void resetPrices() {
+        setPrice(p1Value, 0, p1Price);
+        setPrice(p2Value, 1, p2Price);
+        setPrice(p3Value, 2, p3Price);
+    }
+
+    private String doubleDigitString(int num) {
         String str;
         if (num < 10) {
             str = "0" + num;
         } else {
             str = String.valueOf(num);
         }
-        value.setText(str);
-        setPrice(price, crop, value);
-    }
-
-    private void setPrice(Text price, int crop, Text value) {
-        double basePrice = storageViewModel.userInventory().get(crop).getCropValue();
-        price.setText(String.valueOf(basePrice * Integer.parseInt(value.getText())));
-    }
-
-    private void buyValue(Text value, int crop, Text quantity) {
-        int num = Integer.parseInt(value.getText());
-        double basePrice = storageViewModel.userInventory().get(crop).getCropValue();
-        double budgetCheck = Integer.parseInt(this.budget.getText());
-        if (num * basePrice > budgetCheck && crop <= 2) {
-            marketViewModel.purchaseItems(storageViewModel.userInventory().get(crop), num);
-            quantity.setText(String.valueOf(num));
-        }
-    }
-
-    private void sellValue(Text value) {
-
+        return str;
     }
 
     public void buySwap(MouseEvent mouseEvent) {
@@ -203,6 +381,7 @@ public class MarketPlace {
         setActionLabel("Sell");
         resetValues();
         btnSwap.setText("Buy Crops");
+        resetPrices();
         btnSwap.setOnMouseClicked(this::sellSwap);
     }
 
@@ -213,6 +392,7 @@ public class MarketPlace {
         setActionLabel("Buy");
         resetValues();
         btnSwap.setText("Sell Crops");
+        resetPrices();
         btnSwap.setOnMouseClicked(this::buySwap);
     }
 
@@ -234,156 +414,6 @@ public class MarketPlace {
 
         stage.setTitle("Farm");
         stage.show();
-    }
-
-    public void upValue1() {
-        upValue(p1Value, p1Price, 0);
-    }
-
-    public void downValue1() {
-        downValue(p1Value, p1Price, 0);
-    }
-
-    public void action1() {
-        if (buyState) {
-            buyValue(p1Value, 0, p1Quantity);
-        } else {
-            sellValue(p1Value);
-        }
-    }
-
-    public void upValue2() {
-        upValue(p2Value, p2Price, 1);
-    }
-
-    public void downValue2() {
-        downValue(p2Value, p2Price, 1);
-    }
-
-    public void action2() {
-        if (buyState) {
-            buyValue(p2Value, 1, p2Quantity);
-        } else {
-            sellValue(p2Value);
-        }
-    }
-
-    public void upValue3() {
-        upValue(p3Value, p3Price, 2);
-    }
-
-    public void downValue3() {
-        downValue(p3Value, p3Price, 2);
-    }
-
-    public void action3() {
-        if (buyState) {
-            buyValue(p3Value, 2, p3Quantity);
-        } else {
-            sellValue(p3Value);
-        }
-    }
-
-    public void upValue4() {
-        upValue(p4Value, p4Price, 3);
-    }
-
-    public void downValue4() {
-        downValue(p4Value, p4Price, 3);
-    }
-
-    public void action4() {
-        if (buyState) {
-            buyValue(p4Value, 3, p4Quantity);
-        } else {
-            sellValue(p4Value);
-        }
-    }
-
-    public void upValue5() {
-        upValue(p5Value, p5Price, 4);
-    }
-
-    public void downValue5() {
-        downValue(p5Value, p5Price, 4);
-    }
-
-    public void action5() {
-        if (buyState) {
-            buyValue(p5Value, 4, p5Quantity);
-        } else {
-            sellValue(p5Value);
-        }
-    }
-
-    public void upValue6() {
-        upValue(p6Value, p6Price, 5);
-    }
-
-    public void downValue6() {
-        downValue(p6Value, p6Price, 5);
-    }
-
-    public void action6() {
-        if (buyState) {
-            buyValue(p6Value, 5, p6Quantity);
-        } else {
-            sellValue(p6Value);
-        }
-    }
-
-    public void upValue7() {
-        upValue(p7Value, p7Price, 6);
-    }
-
-    public void downValue7() {
-        downValue(p7Value, p7Price, 6);
-    }
-
-    public void action7() {
-        if (buyState) {
-            buyValue(p7Value, 6, p7Quantity);
-        } else {
-            sellValue(p7Value);
-        }
-    }
-
-    public void upValue8() {
-        upValue(p8Value, p8Price, 7);
-    }
-
-    public void downValue8() {
-        downValue(p8Value, p8Price, 7);
-    }
-
-    public void action8() {
-        if (buyState) {
-            buyValue(p8Value, 7, p8Quantity);
-        } else {
-            sellValue(p8Value);
-        }
-    }
-
-    private void resetValues() {
-        p1Value.setText("01");
-        p2Value.setText("01");
-        p3Value.setText("01");
-        p4Value.setText("01");
-        p5Value.setText("01");
-        p6Value.setText("01");
-        p7Value.setText("01");
-        p8Value.setText("01");
-    }
-
-    private void setActionLabel(String str) {
-        p1Action.setText(str);
-        p2Action.setText(str);
-        p3Action.setText(str);
-        p4Action.setText(str);
-        p5Action.setText(str);
-        p6Action.setText(str);
-        p7Action.setText(str);
-        p8Action.setText(str);
     }
 }
 
